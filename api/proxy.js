8a6ce17cb4eb8eb01dd9b6fa1eb390451a15b85f
Shallrise <1751953786@qq.com>
2022-06-23 22:12:47 +0800

    vercel

--- api/proxy.js
+++ api/proxy.js
@@ -5,10 +5,10 @@ module.exports = (req, res, next) => {
 
     // 代理目标地址
     // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-    if (req.url.startsWith('/api')) {
-        target = 'http://202.193.53.235:8080/'
-            // res.json({ code: 111 });
-    }
+
+    target = 'http://202.193.53.235:8080/'
+        // res.json({ code: 111 });
+
 
     // 创建代理对象并转发请求
     createProxyMiddleware({
