186e60cdba662189d6e9d154379103cc26342b82
Shallrise <1751953786@qq.com>
2022-06-27 21:13:02 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 module.exports = {
     base: "/",
     // theme: 'reco',
-    dest: './dist',
+    dest: '../../dist',
     title: 'Shallrise',
     description: '是我的',
     head: [
