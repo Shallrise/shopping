88de1afaa6077206fad554f5c7f7de1087b332ee
Shallrise <1751953786@qq.com>
2022-06-28 09:36:04 +0800

    push

--- PUSH.bat
+++ PUSH.bat
@@ -2,4 +2,4 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f 'git@github.com:Shallrise/shallrise.github.io.git' gh-pages
+git push -f 'git@github.com:Shallrise/Shallrise.github.io.git' gh-pages
